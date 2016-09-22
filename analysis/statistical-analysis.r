6b05960c54afdca730f1c0d745bb7210d1e18bcd
Rafael Laboissiere <rafael@laboissiere.net>
2016-09-22 18:03:26 +0200

    Add script for doing preliminary statistical analysis

---
+++ analysis/statistical-analysis.r
@@ -0,0 +1,59 @@
+
+### * Statistical analyses
+
+### ** Load the necessary libraries
+
+library (lme4)
+library (lmerTest)
+
+### ** Load the resutls
+
+obj.stab.psycho <- read.csv ("obj-stab-psycho.csv")
+
+### ** Transform the discrete factor chair and object into numeric
+
+obj.stab.psycho$chair.num <- c (0, 1, -1) [as.numeric (obj.stab.psycho$chair)]
+obj.stab.psycho$object.num <- as.numeric(obj.stab.psycho$object) - 2
+
+### ** Tests
+
+### *** Linear mixed models
+
+for (i in seq (1, 4)) {
+
+    if (i == 1) {
+
+        ## **** Efect of chair inclination & object shape in static background
+        df <- subset (obj.stab.psycho,
+                      stimulus == "object" & background == "static")
+        fm <- lmer (threshold ~ object.num * chair.num + (1 | subject), df)
+
+    } else if (i == 2) {
+
+        ## **** Efect of object shape in vection background
+        df <- subset (obj.stab.psycho,
+                      stimulus == "object" & background == "vection")
+        fm <- lmer (threshold ~ object.num + (1 | subject), df)
+
+    } else if (i == 3) {
+
+        ## **** Efect of background in upright position on horizontal estimation
+        df <- subset (obj.stab.psycho,
+                      stimulus == "horizontal" & chair == "upright")
+        fm <- lmer (threshold ~ background + (1 | subject), df)
+
+    } else {
+
+        ## **** Efect of inclination in satic backaground on horizontal estimation
+        df <- subset (obj.stab.psycho,
+                      stimulus == "horizontal" & background == "static")
+        fm <- lmer (threshold ~ chair.num + (1|subject), df)
+
+    }
+
+    show (anova (fm))
+    show (rand (fm))
+    show (fixef (fm))
+    show (ranef (fm))
+
+}
