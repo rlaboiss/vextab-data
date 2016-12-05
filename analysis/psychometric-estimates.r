fac125e4f0559902382a1297ef5a74485bf5866c
Rafael Laboissiere <rafael@laboissiere.net>
2016-12-05 16:56:01 +0100

    a/psychometric-estimates.r: Take into account new fields
    Namely: experiment, object-side, and table-side.

--- analysis/psychometric-estimates.r
+++ analysis/psychometric-estimates.r
@@ -1,21 +1,22 @@
 
 ### * Read the data frame with the responses
 obj.stab <- read.csv ("obj-stab-resp.csv")
+
+### * Get rid of NA in the object column
+obj <- as.character (obj.stab$object)
+obj [which (is.na (obj))] <- "none"
+obj.stab$object <- factor (obj)
+
+### * Generate a column with the numeric representation of responses
 obj.stab$response.n <- as.numeric (obj.stab$response) - 1
 
-### * Compose the data frame for storing the estimated parmaters
-
-### ** First, compose the extensive comnbination of conditions
-obj.stab.psycho <- expand.grid (subject = levels (obj.stab$subject),
-                                background = c ("static", "vection"),
-                                stimulus = c ("object", "horizontal"),
-                                chair = c ("upright", "right", "left"),
-                                object = c ("low", "mid", "high"))
-
-### ** Second, eliminate the non-existent combinations
-obj.stab.psycho <- subset (obj.stab.psycho,
-                           ! (background == "vection" & chair != "upright")
-                           & ! (stimulus == "horizontal" & object != "low"))
+### * Compose the data frame with all possible conditions
+obj.stab.psycho <- aggregate (response ~ subject + experiment + background
+                                  + stimulus + object.side + table.side + chair
+                                  + object,
+                              obj.stab,
+                              function (x) NA,
+                              na.action = na.pass)
 
 ### * Psychometric parameter estimation
 
@@ -46,21 +47,28 @@ threshold <- threshold.ci.inf <- threshold.ci.sup <-
      slope <- slope.ci.inf <- slope.ci.sup <- rep (NA, nrow (obj.stab.psycho))
 
 ### ** Store values for each condition
-for (i in seq (1, nrow (obj.stab.psycho))) {
+nb.cond <- nrow (obj.stab.psycho)
+for (i in seq (1, nb.cond)) {
+
     ## *** Get the condition for each factor
     sb <- as.character (obj.stab.psycho [i, "subject"])
     st <- as.character (obj.stab.psycho [i, "stimulus"])
     bg <- as.character (obj.stab.psycho [i, "background"])
     co <- as.character (obj.stab.psycho [i, "chair"])
     ot <- as.character (obj.stab.psycho [i, "object"])
+    ex <- as.character (obj.stab.psycho [i, "experiment"])
+    os <- as.character (obj.stab.psycho [i, "object.side"])
+    ts <- as.character (obj.stab.psycho [i, "table.side"])
 
     ## *** Create the specific data frame
     df <- subset (obj.stab, subject ==  sb
                             & background == bg
                             & stimulus == st
-                            & chair == co)
-    if (st == "object")
-        df <- subset (df, object == ot)
+                            & chair == co
+                            & experiment == ex
+                            & object.side == os
+                            & table.side == ts
+                            & object == ot)
 
     ## *** Skip the lacking data
     if (all (is.na (df$response)))
@@ -76,7 +84,8 @@ for (i in seq (1, nrow (obj.stab.psycho))) {
     slope.ci.sup [i] <- ps [6]
 
     ## *** Progress meter
-    cat (sprintf ("\r%3d: %s, %6s, %10s, %7s, %4s", i, sb, bg, st, co, ot))
+    cat (sprintf ("\r%5d/%d: %s, %12s, %6s, %10s, %7s, %4s",
+                  i, nb.cond, sb, ex, bg, st, co, ot))
     flush (stdout ())
 }
 
