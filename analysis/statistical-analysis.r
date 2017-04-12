cc4b42ad96898997b84bf44e5de552ab0a02ece9
Rafael Laboissiere <rafael@laboissiere.net>
2017-04-12 19:17:07 +0200

    a/statistical-analysis.r: Fix name of no.table data frame

--- analysis/statistical-analysis.r
+++ analysis/statistical-analysis.r
@@ -513,7 +513,7 @@ no.table <- subset (obj.stab.psycho, experiment == "no-table")
 pdf (file = "no-table-object.pdf")
 par (mar = c (4, 5, 0.5, 0))
 for (i in c (1, 2)) {
-    boxplot (threshold ~ object.num * background * table.side, no.table.obj,
+    boxplot (threshold ~ object.num * background * table.side, no.table,
              frame = FALSE, las = 1, xlab = "", ylab = boxplot.ylab,
              xaxt = "n", pars = boxplot.pars,
              col = rep (obj.col, 2), add = (i == 2))
@@ -527,7 +527,7 @@ legend ("topright", inset = 0.05, pch = 22, pt.cex = 2, pt.bg = obj.col,
 dummy <- dev.off ()
 
 fm <- lmer (threshold ~ background * object.num * table.side + (1 | subject),
-            no.table.obj)
+            no.table)
 show (anova (fm))
 show (fixef (fm))
 show (ranef (fm))
